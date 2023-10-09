e202d94bfaff65d485f167b563529247a184f433
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2023-10-09 09:43:14 +0330

    fix create app modal component

---
+++ components/AppModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <dialog
+    @click="closeModal"
+    id="my_modal_1"
+    :class="`modal ${isMoalOpen&&'modal-open'}`"
+    v-bind="$attrs"
+  >
+    <div @click.stop="" class="modal-box">
+      <slot name="title"
+        ><h3 class="font-bold text-lg">{{title}}</h3></slot
+      >
+
+      <slot name="body"
+        ><p>{{desc }}</p></slot
+      >
+      <div class="modal-action">
+        <!-- {{slots.action&&
+        <form method="dialog">
+          <slot name="action"></slot>
+        </form>
+        }} -->
+        <slot name="action" :closeModal="closeModal"></slot>
+      </div>
+    </div>
+  </dialog>
+</template>
+<script setup lang="ts">
+import { useModal } from '~/composables/useModal';
+const props=defineProps<{
+  title?:string,
+  desc?:string
+}>()
+const {isMoalOpen,closeModal}=useModal()
+</script>
--- pages/base.vue
+++ pages/base.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+
+
 const inputVal=ref('232')
 const handleChange=($event:any)=>{
     inputVal.value=$event.target.value
 }
+const {openModal}=useModal()
 </script>
 <template>
-  <AppTextInput name="title" v-model="inputVal" type="text" placeholder="fsdfasfsf" />
-  <input :value="inputVal" @input="handleChange" placeholder="write sth">
-{{inputVal}}
+  <main>
+    <AppTextInput
+      name="title"
+      v-model="inputVal"
+      type="text"
+      placeholder="fsdfasfsf"
+      size="sm"
+      class="input-primary min-h-[200vh]"
+    />
+    <AppButton @click="openModal" class="">open modal</AppButton>
+    <input :value="inputVal" @input="handleChange" placeholder="write sth" />
+    <!-- <button class="btn" onclick="my_modal_1.showModal()">open modal</button> -->
+    <AppModal title="hello how are you??" desc="what's up my friend?">
+      <template #action="{closeModal}">
+        <button @click="closeModal" class="btn">hello</button>
+      </template>
+    </AppModal>
+  </main>
 </template>
-
-<style scoped></style>
