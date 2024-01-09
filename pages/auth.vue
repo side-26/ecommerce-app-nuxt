dcb81ab26835bd4316f7c5a52709ab6218ab65c8
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2024-01-09 17:16:06 +0330

    fix init new pages

---
+++ pages/auth.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        auth
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style lang="">
+    
+</style>
--- pages/base.vue
+++ pages/base.vue
@@ -1,29 +1,7 @@
 <script setup lang="ts">
-
-
-const inputVal=ref('232')
-const handleChange=($event:any)=>{
-    inputVal.value=$event.target.value
-}
-const {openModal}=useModal()
 </script>
 <template>
   <main>
-    <AppTextInput
-      name="title"
-      v-model="inputVal"
-      type="text"
-      placeholder="fsdfasfsf"
-      size="sm"
-      class="input-primary min-h-[200vh]"
-    />
-    <AppButton @click="openModal" class="">open modal</AppButton>
-    <input :value="inputVal" @input="handleChange" placeholder="write sth" />
-    <!-- <button class="btn" onclick="my_modal_1.showModal()">open modal</button> -->
-    <AppModal title="hello how are you??" desc="what's up my friend?">
-      <template #action="{closeModal}">
-        <button @click="closeModal" class="btn">hello</button>
-      </template>
-    </AppModal>
+    <AppPriceNumber price="3223235345" :needCurrency="true"/>
   </main>
 </template>
