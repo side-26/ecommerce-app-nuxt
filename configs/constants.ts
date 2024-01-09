68a4519ccc421336c0596696699ddff872f3c659
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2024-01-09 17:13:17 +0330

    fix set some constant

---
+++ configs/constants.ts
@@ -0,0 +1,32 @@
+export const appToastKey = '--app_toast--'
+export const CURRENCY='تومان'
+export const appNavbar: {
+    id: number,
+    title: string,
+    to: string
+}[] = [
+        {
+            id: 1
+            ,
+            title: 'صفحه اصلی',
+            to: '/'
+        },
+        {
+            id: 3
+            ,
+            title: 'دوره آموزشی',
+            to: '/courses'
+        },
+        {
+            id: 5
+            ,
+            title: 'مقالات',
+            to: '/articals'
+        },
+        {
+            id: 7
+            ,
+            title: 'درباره ما',
+            to: '/about_us'
+        },
+    ]
