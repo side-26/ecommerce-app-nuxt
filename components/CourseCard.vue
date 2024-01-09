529a4bebf9c431d3212ed59a57d083333fa02ac1
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2024-01-09 17:16:51 +0330

    fix init global card

---
+++ components/CourseCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props=defineProps<{to:string}>()
+</script>
+<template>
+  <NuxtLink :to="to" class="border-[2.5px] border-gray-100 rounded-2xl overflow-hidden block">
+    <div class="">
+      <slot name="header"></slot>
+    </div>
+    <div>
+      <slot name="body"></slot>
+    </div>
+    <div class="">
+      <slot name="actions"></slot>
+    </div>
+  </NuxtLink>
+</template>
