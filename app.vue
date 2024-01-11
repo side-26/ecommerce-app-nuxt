4afe396bab93f6d49b32b11e473d1627ee60a2af
Side-26 <mr72302@gmail>
2024-01-11 20:33:22 +0330

    fix import auth-modal as global modal

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+// @ts-ignore
+import { useAuthModal } from '~/composables/auth/useAuthModal'
+const { isModalOpen, openModal } = useAuthModal()
+</script>
 <template>
   <div dir="rtl">
+    <app-button @click="openModal"> open modal </app-button>
     <NuxtLayout> </NuxtLayout>
-    <TheAppToast />
+    <lazy-the-app-toast />
+    <lazy-auth-modal v-if="isModalOpen" />
   </div>
 </template>
