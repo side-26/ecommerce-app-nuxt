7edd4bc53f7ba64cb63839c31e521de28beb5249
Side-26 <mr72302@gmail>
2024-01-13 21:15:55 +0330

    fix impliment useIdentity and authStore in app root

--- app.vue
+++ app.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
 // @ts-ignore
+import { useToast } from '~/composables/useToast'
+// @ts-ignore
 import { useAuthModal } from '~/composables/auth/useAuthModal'
-const { isModalOpen, openModal } = useAuthModal()
+import { appToastKey } from './configs/constants'
+// @ts-ignore
+import { useAuthStore } from '~/store/Auth.store'
+const { isModalOpen, closeModal } = useAuthModal()
+const { toastInfo } = useToast(appToastKey)
+const authStore = useAuthStore()
+// @ts-ignore
+const { getIdentity } = useIdentity()
+// @ts-ignore
+watchEffect(() => {
+  if (authStore.isLoggedIn) closeModal()
+})
+// @ts-ignore
+onMounted(() => {
+  getIdentity()
+})
 </script>
 <template>
   <div dir="rtl">
-    <app-button @click="openModal"> open modal </app-button>
     <NuxtLayout> </NuxtLayout>
-    <lazy-the-app-toast />
+    <lazy-the-app-toast v-if="toastInfo.showToast" />
     <lazy-auth-modal v-if="isModalOpen" />
   </div>
 </template>
