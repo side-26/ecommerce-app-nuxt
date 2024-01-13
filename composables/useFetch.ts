3876135d61cf4c33e895f29c4972be0ed515cf80
Side-26 <mr72302@gmail>
2024-01-13 21:03:58 +0330

    fix add new configs to use-app-fetch

--- composables/useFetch.ts
+++ composables/useFetch.ts
@@ -1,10 +1,25 @@
-import { BASE_URL } from "~/configs/api.config"
+import { BASE_URL } from "~/configs/api.config";
+import { FetchError, FetchOptions } from "ofetch";
+import { FetchCustomConfig } from "~/types/fetch";
+import { useAuthStore } from "~/store/Auth.store";
 
+export const useFetchData = <R, T>() => {
+  const authStore = useAuthStore();
 
-export const useFetchData = () => {
-    const getFetchedData = <T>(url: string, config?: NitroFetchOptions) => {
-        
-        return $fetch<T>(url, { baseURL: BASE_URL, ...config })
+  const customFetch = (
+    url: string,
+    config: FetchOptions,
+    customConfig: FetchCustomConfig = {}
+  ) => {
+    if (customConfig.setToken) {
+      if (!config.headers) config.headers = {};
+      config.headers["Authorization"] = `Bearer ${authStore.getToken}`;
     }
-    return getFetchedData
-}
+    // @ts-ignore
+    return $fetch<T>(url, { baseURL: BASE_URL, ...config })
+      .then((res) => res)
+      .catch((e) => customConfig?.onError?.(e));
+  };
+
+  return customFetch;
+};
