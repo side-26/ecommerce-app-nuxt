dea49a2e8d138d98c6ce52cc543be91065041053
Side-26 <mr72302@gmail>
2024-01-13 21:08:15 +0330

    fix add new constant

--- configs/constants.ts
+++ configs/constants.ts
@@ -1,32 +1,36 @@
-export const appToastKey = '--app_toast--'
-export const CURRENCY='تومان'
+export const appToastKey = "--app_toast--";
+export const CURRENCY = "تومان";
+export const TOKEN_KEY = "token";
+export const ROUTES = {
+  home: "/",
+  courses: "/courses",
+  articals: "/articals",
+  aboutUs: "/about_us",
+  profile: "/profile",
+};
 export const appNavbar: {
-    id: number,
-    title: string,
-    to: string
+  id: number;
+  title: string;
+  to: string;
 }[] = [
-        {
-            id: 1
-            ,
-            title: 'صفحه اصلی',
-            to: '/'
-        },
-        {
-            id: 3
-            ,
-            title: 'دوره آموزشی',
-            to: '/courses'
-        },
-        {
-            id: 5
-            ,
-            title: 'مقالات',
-            to: '/articals'
-        },
-        {
-            id: 7
-            ,
-            title: 'درباره ما',
-            to: '/about_us'
-        },
-    ]
+  {
+    id: 1,
+    title: "صفحه اصلی",
+    to: ROUTES.home,
+  },
+  {
+    id: 3,
+    title: "دوره آموزشی",
+    to: ROUTES.courses,
+  },
+  {
+    id: 5,
+    title: "مقالات",
+    to: ROUTES.articals,
+  },
+  {
+    id: 7,
+    title: "درباره ما",
+    to: ROUTES.aboutUs,
+  },
+];
