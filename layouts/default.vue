878ecc0d285789b60a598dcc14db49719c61c18e
Side-26 <mr72302@gmail>
2024-01-13 21:13:44 +0330

    fix add auth logic for showing account btn or login-register btn to the client

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import { appNavbar } from '~/configs/constants'
+import { useLogout } from '~/composables/auth/useLogout'
+import { useAuthStore } from '~/store/Auth.store'
+import { ROUTES } from '~/configs/constants'
+const authStore = useAuthStore()
+const { logout } = useLogout()
+// @ts-ignore
 const { isMobile, isDesktop, isWindows } = useDevice()
 const isMenuOpen = ref<boolean>(false)
+const isAccountMenuOpen = ref<boolean>(false)
 const toggleMenu = () => {
   isMenuOpen.value = !unref(isMenuOpen)
 }
@@ -42,14 +49,47 @@ const toggleMenu = () => {
             />
           </svg>
         </app-button>
-        <ul class=" flex items-center text-lg">
-          <li class="lg:ml-4 px-3">
-            <NuxtLink to="/auth" class="no-animation text-primary text-base">
-              ورود \ ثبت نام</NuxtLink
-            >
-          </li>
+        <ul class="flex items-center text-lg">
+          <client-only>
+            <li class="lg:ml-4 px-3">
+              <NuxtLink
+                v-if="!authStore.isLoggedIn"
+                :to="{ path: '/', query: { need_auth: 'true' } }"
+                class="no-animation text-primary text-base"
+              >
+                ورود \ ثبت نام</NuxtLink
+              >
+              <template v-else>
+                <app-button
+                  variant="ghost"
+                  @click="isAccountMenuOpen = !isAccountMenuOpen"
+                  class="relative"
+                >
+                  {{ authStore.getFullName }}
+                </app-button>
+                <ul
+                  v-show="isAccountMenuOpen"
+                  class="flex flex-col gap-1 absolute -bottom-[105px] transition bg-white z-20 rounded-md overflow-hidden shadow-sm"
+                >
+                  <li class="border-b-[1px] border-gray-200 p-3">
+                    <nuxt-link :to="ROUTES.profile" class="text-center text-sm">
+                      پروفایل
+                    </nuxt-link>
+                  </li>
+                  <li class="border-b-[1px] border-gray-200 p-3">
+                    <nuxt-link
+                      @click="logout"
+                      class="text-center text-sm text-red-500"
+                    >
+                      خروج از حساب کاربری
+                    </nuxt-link>
+                  </li>
+                </ul>
+              </template>
+            </li>
+          </client-only>
           <li class="hidden md:block">
-            <NuxtLink to="/">اکادمی لند</NuxtLink>
+            <nuxt-link to="/">اکادمی لند</nuxt-link>
           </li>
         </ul>
       </nav>
@@ -70,7 +110,7 @@ const toggleMenu = () => {
       </ul>
     </header>
     <main class="flex-grow">
-      <NuxtPage />
+      <nuxt-page />
     </main>
     <footer class="pt-5">
       <p class="flex justify-center gap-1 text-lg px-1 py-2 bg-slate-200">
@@ -90,8 +130,8 @@ const toggleMenu = () => {
             stroke-linejoin="round"
             d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z"
           />
-        </svg> 
-        و 
+        </svg>
+        و
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -106,3 +146,14 @@ const toggleMenu = () => {
     </footer>
   </section>
 </template>
+<style lang="css">
+.account_navbar {
+  transform: scale(0);
+}
+.account_btn:hover + .account_navbar {
+  transform: scale(1);
+}
+.account_navbar:hover {
+  transform: scale(1);
+}
+</style>
