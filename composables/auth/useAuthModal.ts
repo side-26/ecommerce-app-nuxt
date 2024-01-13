4b35a08d8496f6d44e68ca82a3e32dd630be725b
Side-26 <mr72302@gmail>
2024-01-13 21:09:05 +0330

    fix add new logic for opening and closing

--- composables/auth/useAuthModal.ts
+++ composables/auth/useAuthModal.ts
@@ -1,10 +1,16 @@
+import { useAuthStore } from "~/store/Auth.store";
+
 export const useAuthModal = () => {
+  const route = useRoute();
   const isModalOpen = useState<boolean>("--auth-modal--", () => false);
+  const router = useRouter();
   const openModal = () => {
     isModalOpen.value = true;
   };
-  const closeModal=()=>{
-    isModalOpen.value=false
-  }
-  return { isModalOpen,openModal,closeModal };
+  const closeModal = () => {
+    if (route.fullPath === "/?need_auth=true")
+      router.replace({ path: "/", query: {} });
+    isModalOpen.value = false;
+  };
+  return { isModalOpen, openModal, closeModal };
 };
