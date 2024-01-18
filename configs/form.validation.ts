f9c1e6dc8296709cee70b62e53d158706c30f32c
Side-26 <mr72302@gmail>
2024-01-18 20:56:00 +0330

    fix add new validation schema

--- configs/form.validation.ts
+++ configs/form.validation.ts
@@ -4,3 +4,9 @@ export const loginSchema = object({
   username: string().required().label("نام کاربری"),
   password: string().required().label("پسورد"),
 });
+export const commentSchema = object({
+  first_name: string().required().label("اسم"),
+  last_name: string().required().label("نام خانوادگی"),
+  comment: string().required().label("نظر"),
+});
+// TODO add other commentSchema
