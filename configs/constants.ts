6b3f0762fe09f40c7d2902977aaed79278ac1ed8
Side-26 <mr72302@gmail>
2024-01-18 20:55:05 +0330

    fix add new const

--- configs/constants.ts
+++ configs/constants.ts
@@ -34,3 +34,35 @@ export const appNavbar: {
     to: ROUTES.aboutUs,
   },
 ];
+export const courseDetailsNavbar = [
+  {
+    id: 1,
+    title: "درباره دوره",
+    to: "#about_course",
+    elementId: "about_course",
+  },
+  {
+    id: 2,
+    title: "پیش نیازهای دوره",
+    to: "#prerequisite_courses",
+    elementId: "prerequisite_courses",
+  },
+  {
+    id: 3,
+    title: "لیست ویدیو ها",
+    to: "#courses_list",
+    elementId: "courses_list",
+  },
+  {
+    id: 4,
+    title: "پرسش های متداول",
+    to: "#FAQ",
+    elementId: "FAQ",
+  },
+  {
+    id: 5,
+    title: "نظرات کاربران",
+    to: "#comments",
+    elementId: "comments",
+  },
+];
