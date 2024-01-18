5df8b5ec8e9ee37806c1d8f1ef3018aa30ff14a3
Side-26 <mr72302@gmail>
2024-01-18 20:50:08 +0330

    fix init app-image

---
+++ components/AppImage.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Ref } from 'vue'
+
+const props = defineProps<{
+  src: string
+  containerClass: string
+}>()
+const imgSrc: Ref<string> = computed(() => {
+  if (props.src) return props.src
+  return ''
+})
+const isLocalImg = computed(() => {
+  if (props.src) return !!props?.src?.includes('assets')
+  return false
+})
+</script>
+<template>
+  <figure :class="containerClass + ' overflow-hidden'">
+    <img :src="imgSrc" loading="lazy" v-bind="$attrs" class="h-full w-full" />
+    <!-- <nuxt-img
+      v-else
+      loading="lazy"
+      :src="imgSrc"
+      v-bind="$attrs"
+      class="h-full w-full"
+    /> -->
+  </figure>
+</template>
+<style lang=""></style>
