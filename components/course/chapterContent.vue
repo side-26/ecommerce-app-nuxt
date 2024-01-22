82b9e6df09b4a99d10c87b599b0c5e12eaaa939f
Side-26 <mr72302@gmail>
2024-01-22 22:41:10 +0330

    fix init a chapter-item and chapter-content

---
+++ components/course/chapterContent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { CourseChaperWithCounter } from '~/types/course'
+const props = defineProps<{
+  item: CourseChaperWithCounter
+}>()
+const firstItemId = computed(() => props?.item[0]?.courseVideos[0]?.id)
+console.log(firstItemId)
+</script>
+<template lang="">
+  <app-collaps
+    v-for="{ chapterName, courseVideos, id } in item"
+    :key="id"
+    :title="chapterName"
+  >
+    <template #body>
+      <course-chapter-item
+        v-for="{ duration, hasFile, isDemo, videoTitle, id } in courseVideos"
+        :key="id"
+        :id="id"
+        :duration="duration"
+        :has-file="hasFile"
+        :is-demo="isDemo"
+        :video-title="videoTitle"
+        :first-item-id="firstItemId"
+      />
+    </template>
+  </app-collaps>
+</template>
