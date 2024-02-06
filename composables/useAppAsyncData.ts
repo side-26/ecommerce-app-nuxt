27b2ad1f9ed746e66c591b45e7ec62329fdbeaed
Side-26 <mr72302@gmail>
2024-02-06 20:00:25 +0330

    fix impliment initial cache iside useAppAsync wrapper

--- composables/useAppAsyncData.ts
+++ composables/useAppAsyncData.ts
@@ -1,34 +1,44 @@
-import { AsyncData, AsyncDataOptions } from "nuxt/app";
-import { NuxtApp } from "nuxt/schema";
-
+import type { AsyncData, AsyncDataOptions } from "nuxt/app";
+import type { AsyncDataExecuteOptions } from "nuxt/dist/app/composables/asyncData";
+import type { NuxtApp } from "nuxt/schema";
 export const useAppAsyncData = async <DataT, DataE>(
   key: string,
   handler: (ctx?: NuxtApp | undefined) => Promise<DataT>,
   options?: AsyncDataOptions<DataT>,
   initialCache: boolean = true
-): Promise<AsyncData<DataT, DataE>> => {
+): Promise<AsyncData<DataT, Ref<DataE | null>>> => {
+  const nuxtApp = useNuxtApp();
   const { data, error, execute, pending, refresh, status } = useAsyncData<
     DataT,
     DataE
     // @ts-ignore
-  >(key, handler, { immediate: process.server, ...options });
-  // initial-cache
-  
+  >(key, handler, {
+    getCachedData(key) {
+      if (initialCache)
+        return nuxtApp.payload?.data[key] || nuxtApp?.static?.data[key];
+    },
+    ...options,
+  });
+  // console.log('useappasynce')
   // error-handling
-  // if (options?.server === true) {
-  //   if (unref(error)?.["statusCode"] || unref(error)?.["statusMessage"]) {
-  //     showError({
-  //       statusMessage: unref(error)?.["statusMessage"],
-  //       statusCode: unref(error)?.["statusCode"],
-  //     });
-  //   }
-  // }
+  if (options?.server === true) {
+    if (unref(error)?.["statusCode"] || unref(error)?.["statusMessage"]) {
+      showError({
+        statusMessage: unref(error)?.["statusMessage"],
+        statusCode: unref(error)?.["statusCode"],
+      });
+    }
+  }
   return {
     data: data as Ref<DataT>,
-    error,
-    execute,
+    error: error as Ref<Ref<DataE | null> | null>,
+    execute: execute as (
+      opts?: AsyncDataExecuteOptions | undefined
+    ) => Promise<DataT>,
     pending,
-    refresh,
+    refresh: refresh as (
+      opts?: AsyncDataExecuteOptions | undefined
+    ) => Promise<DataT>,
     status,
   };
 };
