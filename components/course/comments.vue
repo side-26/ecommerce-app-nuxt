84e201c37983106c25f4ae8b916572c84fd17146
Side-26 <mr72302@gmail>
2024-02-14 12:48:06 +0330

    fix add a button in comments section for atract user to login

--- components/course/comments.vue
+++ components/course/comments.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 // @ts-ignore
 import { useGetComments } from '~/composables/course/useComments'
+import { useAuthStore } from '~/store/Auth.store'
 const props = defineProps<{
   courseId: number | undefined
 }>()
+const router = useRouter()
+const authStore = useAuthStore()
 // @ts-ignore
-
 const { data: commentsData, pending } = await useGetComments(props.courseId)
+const goToLogin = () => {
+  router.replace('/?need_auth=true')
+}
 </script>
 <template lang="">
-  <lazy-course-comment-form :courseId="courseId" />
+  <div v-if="!authStore.isLoggedIn" class="my-3">
+    <div @click="goToLogin" role="button" class="alert alert-info text-sm">
+      برای ثبت نظر ابتدا وارد شوید
+    </div>
+  </div>
+  <lazy-course-comment-form v-if="authStore.isLoggedIn" :courseId="courseId" />
   <lazy-course-comments-list
     v-if="commentsData"
     :comments-list="commentsData"
