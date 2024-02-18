cedc9fd5a51f2132e485b77ace4f722125cc8bdf
Side-26 <mr72302@gmail>
2024-02-18 19:05:33 +0330

    fix remove useless composables

--- composables/auth/useRefreshToken.ts
+++
@@ -1,9 +0,0 @@
-import { useAuthStore } from "~/store/Auth.store";
-import { useRefreshTokenService } from "./useRefreshToken.service";
-
-export const useRefreshToken = () => {
-  const doRefreshTokenService = (refreshToken: string) => {
-    console.log("this is refreshTokken", refreshToken);
-  };
-  return doRefreshTokenService;
-};
