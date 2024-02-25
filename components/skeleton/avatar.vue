30a504fa76ef145799f017ea88a8196304c9d7f4
Side-26 <mr72302@gmail>
2024-02-25 21:28:35 +0330

    fix add some skeletons

---
+++ components/skeleton/avatar.vue
@@ -0,0 +1,4 @@
+
+<template lang="html">
+  <div class="animate-pulse w-full rounded-full bg-slate-400"></div>
+</template>
---
+++ components/skeleton/text.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+<template lang="html">
+  <div class="animate-pulse rounded-md bg-slate-400"></div>
+</template>
+<style lang=""></style>
